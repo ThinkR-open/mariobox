0e4c6bcc1dfdbebf3aadce110816564e074bb1ac
vincent <vincent@thinkr.fr>
2023-11-21 13:23:45 +0100

    allow user to overwrite host parameter

--- inst/marioboxexample/R/run_plumber.R
+++ inst/marioboxexample/R/run_plumber.R
@@ -3,9 +3,12 @@
 #' Read the yaml file listing all endpoints and
 #' deploy them
 #'
+#' @param host The IPv4 address that the plumber should listen on.
+#' Defaults to the plumber.host option, if set, or "127.0.0.1" if not. 
+#'
 #' @export
-run_api <- function() {
-  generate_api()$run()
+run_api <- function(host = getOption("plumber.host",default = "127.0.0.1")) {
+  generate_api()$run(host = host)
 }
 
 generate_api <- function() {
