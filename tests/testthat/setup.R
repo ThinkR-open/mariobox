f11596a4195d6e1f38b26f16d1b8301f9bde1261
colin <colin@thinkr.fr>
2024-12-10 09:26:20 +0100

    test: helper is now setup

---
+++ tests/testthat/setup.R
@@ -0,0 +1,32 @@
+run_quietly_in_a_dummy_mariobox <- function(expr) {
+  on.exit(
+    {
+      dir_remove(
+        dummy_mariobox
+      )
+    },
+    add = TRUE
+  )
+  withr::with_options(
+    c("usethis.quiet" = TRUE),
+    {
+      dummy_mariobox <- tempdir()
+      dummy_mariobox <- create_mariobox(
+        dummy_mariobox,
+        overwrite = TRUE
+      )
+      withr::with_dir(
+        dummy_mariobox,
+        expr
+      )
+    }
+  )
+}
+
+dir_remove <- function(path) {
+  unlink(
+    x = path,
+    recursive = TRUE,
+    force = TRUE
+  )
+}
